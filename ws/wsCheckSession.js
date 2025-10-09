f456dc3936d13416a3d4053b1192b672eee21494
Laurent Dubertrand <laurent@dubertrand.com>
2025-10-09 19:59:55 +0200

    removed old js files

--- ws/wsCheckSession.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function ( socket, req) {
-    const response = { session_active: true };
-
-        if (!req.session.TornAPIKey )
-            response.session_active = false;
-
-        socket.send(JSON.stringify(response));
-}
